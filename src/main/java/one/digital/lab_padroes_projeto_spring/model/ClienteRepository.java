95bc942266a51a49e9a4e47f3bc6c4b894460b96
Gustavo Araujo <guucarlos18@gmail.com>
2025-05-11 18:54:03 -0300

    Primeiro commit do projeto

---
+++ src/main/java/one/digital/lab_padroes_projeto_spring/model/ClienteRepository.java
@@ -0,0 +1,10 @@
+package one.digital.lab_padroes_projeto_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long>{
+	
+
+}
